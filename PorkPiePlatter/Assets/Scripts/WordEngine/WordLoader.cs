e7e2fad8a798568c0aa535e601fff22e455c13d7
Shankar <shnkr@live.in>
2018-04-17 02:22:53 +0530

    Base Project

---
+++ PorkPiePlatter/Assets/Scripts/WordEngine/WordLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordLoader
+{
+    public string[] LoadWordsFromFile(string a_FilePath)
+    {
+        if (!string.IsNullOrEmpty(a_FilePath))
+        {
+            TextAsset asset = Resources.Load<TextAsset>(a_FilePath);
+            if (asset != null)
+            {
+                string combinedWords = asset.text;
+                if (!string.IsNullOrEmpty(combinedWords))
+                {
+                    string[] words = combinedWords.Split('\n');
+                    if (words.Length > 0)
+                    {
+                        return words;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
